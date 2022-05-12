557d766192548a118b9abe640799b41101d7a409
“nehatareygit config --global user.email “nehatarey@gmail.com <ntarey26@gmail.com>
2022-05-12 19:59:17 -0400

    hugging face change

--- assignments/Week_13/FastAPI_hello_world/main.py
+++ assignments/Week_13/FastAPI_hello_world/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+
+from transformers import pipeline
+sentiment_model = pipeline("sentiment-analysis")
+
+#sentiment_query_sentence = get_random_comment("Hello World")
+#sentiment = sentiment_model(sentiment_query_sentence)
+#print(f"Sentiment test: {sentiment_query_sentence} == {sentiment})
+
+from pydantic import BaseModel
+
+class PredictionRequest(BaseModel):
+  query_string: str
+
+
+@app.post("my-endpoint")
+def my_endpoint(request: PredictionRequest):
+  # YOUR CODE GOES HERE 
+  return sentiment_model(["I love you", "I hate you"]) 
+
 @app.get("/health")
 def health():
     return "Service is online."
